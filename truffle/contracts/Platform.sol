bd32a0fcb741768b7679612104ddff3ed405490d
Matteo Razzanelli <matteo.razzanelli89@gmail.com>
2022-08-18 17:29:36 +0200

    wip smart contract for charity auctions

--- truffle/contracts/Platform.sol
+++ truffle/contracts/Platform.sol
@@ -1,195 +1,168 @@
+//SPDX-License-Identifier: UNLICENSED
 
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/// @title Charity auctions for limited edition shoes
 /// @author Matteo Razzanelli
 
 import "./parents/SafeMath.sol";
 import "./parents/Governed.sol";
 
 /**
- * @title Notary Token contract
- * @dev This is a ERC20 Token.
- * 
- * The token has no initial supply but token are generated when:
- * - contract have been completed;
- * - new user added (to incentivize)
- *
- * The platform owns the contract.
- */
-contract NotaryToken is Governed {
+* @title Auctioneer contract
+* @dev This is the implementation of the the auctioneers.
+*/
+
+contract Auctioneer is Governed {
 
   // States
-  address payable owner_;
-  mapping(address => bool) private users_;
+  mapping(address => bool) private auctioneers_;
 
   // Events
-  event UserAdded(address indexed account);
-  event UserRemoved(address indexed account);
+  event AuctioneerAdded(address indexed account);
+  event AuctioneerRemoved(address indexed account);
 
-  // Constructor
-  constructor() ERC20("Notary Token", "NTT") {
-    owner_ = payable(msg.sender);
-    _mint(msg.sender, 0);
-  }
-
-  // Access control modifier
-  modifier onlyOwner {
-    require(msg.sender == owner_, "Only the contract owner_ can call this function");
+  // Modifier
+  modifier onlyAuctioneer() {
+    require(isAuctioneer(msg.sender), "Only auctioneer can call");
     _;
   }
 
-  // Contract destructor
-  function destroy() public onlyOwner {
-    selfdestruct(owner_);
-  } 
-
-  // Add a new client
-  function addUser(address user) public {
-    if(!users_[user]){ // add only once
-      users_[user] = true;
-      _mint(user, 1); // mint a token when adding user
-      emit UserAdded(user);
-    }
+  // Set the owner as the first auctioneer
+  constructor () {
+    Governed._initialize(msg.sender);
+    addAuctioneer(msg.sender);
   }
 
-  // Remove a client
-  function removeUser(address user) public onlyOwner {
-    users_[user] = false;
-    _burn(user, balanceOf(user)); // burn the token previously created
-    emit UserRemoved(user);
+  // Add a new auctioneer.
+  function addAuctioneer(address account) public onlyGovernor {
+    auctioneers_[account] = true;
+    emit AuctioneerAdded(account);
   }
 
-  // Return if the user is a client or not.
-  function isUser(address user) public view returns (bool) {
-    return users_[user];
+  // Remove a auctioneer.
+  function removeAuctioneer(address account) public onlyGovernor {
+    auctioneers_[account] = false;
+    emit AuctioneerRemoved(account);
   }
 
+  // Return true if `account` is a auctioneer.
+  function isAuctioneer(address account) public view returns (bool) {
+    return auctioneers_[account];
+  }
 }
 
 /**
-* @title Notary platform contract
-* @dev This is the implementation of the notary platform associated with the token.
+* @title Auctions contract
+* @dev This is the implementation of the the auctions service.
 */
-contract NotaryContract is NotaryToken {
+
+contract Auctions is Auctioneer {
 
   using SafeMath for uint;
 
-  // The platform is idenified by the set of contracts, each contract as a proper structure
-  struct TitleDeed {
-    address payable buyer;
-    address payable seller;
+  struct Auction {
+    address payable beneficiary;
     string description;
-    uint amount;
-    uint amount_for_now;
     uint deadline;
+    mapping( address => uint ) offers;
+    uint numOffers;
+    uint highestBid;
+    address highestBidder;
     bool completed;
   }
-  uint public numContract_ = 0;
-  uint public constant minPeriodOfDeadline_ = 1 weeks;
-  mapping (uint => TitleDeed) public acts_;
+
+  uint public numAuctions_ = 0;
+  mapping( uint => Auction ) public auctions_;
+  string public companyName_;
+
+  mapping( uint => string) public receipts;
+  uint public numReceipts;
 
   // Events
-  event NewContractCreated(uint indexed actID, address indexed buyer, address indexed seller, uint amount);
-  event NewContribution(uint indexed actID, address indexed from, uint indexed amount);
-  event GoalReached(uint indexed actID, uint indexed timestamp, uint amount);
-  event ActFailed(uint indexed actID, uint indexed timestamp, uint amount);
-  event GeneralDeposit(address indexed from, uint amount);
+  event NewAuctionCreated(uint indexed auctionID, address beneficiary, uint indexed deadline, uint indexed startingPrice);
+  event NewHighestBid(uint indexed auctionID, address indexed bidder, uint indexed amount);
+  event AuctionFailed(uint indexed auctionID, address indexed winner, uint indexed amount, address beneficiary);
 
-  event Balances(address indexed from, uint amount);
-
-  // Constructor
-  constructor () NotaryToken () {}
-
-  // Add a new contract
-  function newContract(address payable buyer, address payable seller, 
-    string memory description, uint amount, uint deadline) public {
-
-    // Initial check
-    require(buyer != address(0), "Zero address entered for the buyer!");
-    require(seller != address(0), "Zero address entered for the seller!");
-    require(amount > 0, "Amount of the contract has to be a positive value");
-    require(deadline > block.timestamp, "Set a future deadline!");
-    require(deadline.sub(block.timestamp) > minPeriodOfDeadline_, "Set a greater deadline!");
-
-    NotaryToken.addUser(buyer);
-    NotaryToken.addUser(seller);
-
-    uint actID = numContract_++;
-
-    acts_[actID] = TitleDeed (buyer, seller, description, amount, 0, deadline, false);
-
-    emit NewContractCreated(actID, acts_[actID].buyer, acts_[actID].seller, acts_[actID].amount);
+  // Initialize variables
+  constructor(string memory companyName) Auctioneer() {
+    companyName_ = companyName;
   }
 
-  // Contribute in an act of sale
-  function newPayment(uint actID) public payable {
-    require(acts_[actID].completed == false, "Contract has been already completed!");
-    require(acts_[actID].amount > 0, "ActID is not correct!");
-    require(acts_[actID].deadline > block.timestamp, "Deadline has already been reached!");
-    require(msg.value > 0, "Offer value has to be greater than zero");
+  // Create a new charity auction
+  function newAuction( address payable beneficiary, string memory description, uint startingPrice, uint deadline ) external onlyAuctioneer {
+    require(beneficiary != address(0), "Zero address entered");
+    require(startingPrice >= 0, "Starting price has to be positive or null");
+    require(deadline > 0, "Available time to bid has to be greather than zero");
 
-    acts_[actID].amount_for_now = acts_[actID].amount_for_now.add(msg.value);
+    uint auctionID = numAuctions_++;
+    auctions_[auctionID] = Auction ({
+      beneficiary: beneficiary,
+      description: description, 
+      deadline : block.timestamp.add(deadline), 
+      numOffers: 0,
+      highestBid: startingPrice,
+      completed: false
+    });
+    emit NewAuctionCreated(auctionID, beneficiary, deadline, startingPrice);
+  }
 
-    emit NewContribution(actID, msg.sender, msg.value);
+  // New offer
+  function newOffer(uint auctionID) external payable {
 
-    // Reward contributor with tokens proportional to contribution
-    ERC20._mint(msg.sender, msg.value*1/10 wei);
+    require(auctions_[auctionID].completed == false, "Auction has already ended!");
+    // require(msg.value > auctions[auctionID].highestBid, "Your bid value is lower than the highest bid");
+    require(a.deadline > 0, "AuctionID is not correct!");
 
-    // Check if contract has been completed
-    if(acts_[actID].amount <= acts_[actID].amount_for_now) {
-      acts_[actID].completed = true;
-      (bool success, ) = acts_[actID].seller.call{value: acts_[actID].amount_for_now}("");
-      require(success, "Transfer failed.");
-      acts_[actID].amount_for_now = 0;
+    // Increase sender already offer
+    auctions[auctionID].offers[msg.sender] = auctions[auctionID].offers[msg.sender].add(msg.value);
+    auctions[auctionID].numOffers++;
 
-      // Reward buyer with further NTT tokens
-      ERC20._mint(acts_[actID].buyer, 1000 wei);
-
-      // Notify goal reached
-      emit GoalReached(actID, block.timestamp, acts_[actID].amount_for_now);
+    if(auctions[auctionID].highestBid < msg.value){
+      auctions[auctionID].highestBid = msg.value;
+      auctions[auctionID].highestBidder = msg.sender;
+      emit HighestBidIncreased(auctionID, msg.sender, msg.value);
+    }
+    else{
+      payable(address(msg.sender)).transfer(msg.value);
     }
   }
 
-  // Check if goal reached
-  function checkGoalReached(uint actID) public view returns (bool) {
-    return acts_[actID].completed;
+    
+
+  /**
+    * @dev End an auction, transfer the highest bid to beneficiary and store hash of json receipt, only auctioneers
+    * @param auctionID ID associated with the auction
+    * @param hash string of json auction receipt
+    */
+  function auctionEnd(uint auctionID, string memory hash) external onlyAuctioneer{
+
+      // Checks
+      Auction storage a = auctions[auctionID];
+      require(a.deadline > 0, "AuctionID is not correct!");
+      require(bytes(hash).length != 0, "Hash string not passed");
+      require(block.timestamp > a.deadline, "Auction has not reached its deadline yet");
+      require(a.completed == false, "Auction has been already completed");
+
+
+      // Effects
+      a.completed = true;
+      uint amount = a.highestBid;
+      a.highestBid = 0;
+
+      // Interaction
+      a.beneficiary.transfer(amount);
+
+      // Store hash
+      receipts[auctionID] = hash;
+      numReceipts++;
+
+      emit AuctionEnded(auctionID, a.highestBidder, amount, a.beneficiary);
   }
 
-  // Refund policy
-  function deletedNotaryAct(uint actID) public {
-    require(acts_[actID].completed == false, "Contract has been successfully completed!");
-
-    // For simplicity only the original buyer is refunded
-    uint amountFunded = acts_[actID].amount_for_now;
-
-    require(address(this).balance >= amountFunded,"Insufficient balance in faucet for withdrawal request");
-    acts_[actID].buyer.transfer(amountFunded); // this transfers to buyer.balance NOT token
-
-    acts_[actID].completed = true;
-
-    emit ActFailed(actID, block.timestamp, amountFunded);
+    // We have to create our own getter
+  function getTitleDeed(uint auctionID) public view returns (Auction memory) {
+    return acts_[auctionID];
   }
 
-  function getBuyer(uint actID) external view returns (address payable) {
-    return acts_[actID].buyer;
-  }
-
-  function getSeller(uint actID) external view returns (address payable) {
-    return acts_[actID].seller;
-  }
-
-  function getDeadline(uint actID) external view returns (uint) {
-    return acts_[actID].deadline;
-  }
-
-  // We have to create our own getter
-  function getTitleDeed(uint actID) public view returns (TitleDeed memory) {
-    return acts_[actID];
-  }
-
-  // Fallback function: accept any incoming amount
-  receive() external payable {
-    emit GeneralDeposit(msg.sender, msg.value);
-  }
 }

e4c4cb3b756def2237144a53c1c996ab27598b52
Matteo Razzanelli <matteo.razzanelli89@gmail.com>
2022-08-23 08:10:20 +0200

    fix: connection to ethereum by using metamask is ok

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,10 @@
 // This function detects most providers injected at window.ethereum
-import detectEthereumProvider from '@metamask/detect-provider';
+// import detectEthereumProvider from '@metamask/detect-provider';
 
 // Handle button
 const sendRequestButton = document.getElementById('sendRequestButton');
-const connectButton = document.getElementById('connectButton');
+const connectButton = document.getElementById('connectButton', connect);
+connectButton.addEventListener('click', connect);
 
 // Handle events
 ethereum.on('accountsChanged', handleAccountsChanged);
@@ -18,62 +19,47 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // Check for a provider
 async function init() {
+  // this returns the provider, or null if it wasn't detected
   const provider = await detectEthereumProvider();
   if (provider) {
     console.log('Ethereum successfully detected!')
-    startApp(provider);
+    startApp(provider);  // Initialize your app
   } else {
-  // Warn
-  console.error('MetaMask is not installed');
-  connectButton.innerText = 'Metamask is not available';
-  connectButton.style.backgroundColor = "grey";
-  connectButton.style.color = 'black';
-  connectButton.style.border = '1px solid black'
-  connectButton.style.cursor = "not-allowed";
-  alert('Please install MetaMask to access the page properly');
+    console.error('MetaMask is not installed');
+    connectButton.innerText = 'Metamask is not available';
+    connectButton.disabled = true;
   }
 }
 
 // Permit connection with MetaMask if it is installed
 async function startApp(provider) {
+  // If the provider returned by detectEthereumProvider is not the same as
+  // window.ethereum, something is overwriting it, perhaps another wallet.
   if (provider !== window.ethereum) {
     console.error('Do you have multiple wallets installed?');
     connectButton.innerText = 'Metamask is not available';
-    connectButton.style.backgroundColor = "grey";
-    connectButton.style.color = 'black';
-    connectButton.style.border = '1px solid black'
-    connectButton.style.cursor = "not-allowed";
+    connectButton.disabled = true;
     alert('Use a single wallet to access the page properly');
   }
-  // if ok, do a request
-  else
-    await ethereum
-      .request({ method: 'eth_requestAccounts' })
-      .then(handleAccountsChanged)
-      .catch((err) => {
-        if (err.code === 4001) {
-          // EIP-1193 userRejectedRequest error
-          // If this happens, the user rejected the connection request.
-          console.log('Please connect to MetaMask.');
-        } else {
-          console.error(err);
-        }
-      });
+  else 
+    console.log('Only one wallet: ok!');
+}
 
-    // In case we need to handle the smart contract directly here in js...
-    // try {
-    //   // Initialize web3
-    //   web3 = new Web3(window.ethereum);
-    //   web3.eth.defaultAccount = currentAccount;
-    //   web3.eth.Contract.transactionPollingTimeout = 60*5;
-  
-    //   // Reference to the smart contract
-    //   contract = new web3.eth.Contract(abi, contractAddress);
-    //   contract.transactionPollingTimeout = 60*5;
-    // }
-    // catch (error) {
-    //   console.error(error);
-    // }
+// You should only attempt to request the user's accounts in response to user interaction
+function connect() {
+  console.log("Connecting...");
+  ethereum
+    .request({ method: 'eth_requestAccounts' })
+    .then(handleAccountsChanged)
+    .catch((err) => {
+      if (err.code === 4001) {
+        // EIP-1193 userRejectedRequest error
+        // If this happens, the user rejected the connection request.
+        console.log('Please connect to MetaMask.');
+      } else {
+        console.error(err);
+      }
+    });
 }
 
 function handleAccountsChanged(accounts) {
@@ -83,10 +69,11 @@ function handleAccountsChanged(accounts) {
     location.reload();
   } else {
     // Set current account
+    console.log("Setting account...");
     currentAccount = accounts[0];
     console.log(currentAccount);
     sendRequestButton.disabled = false;
-    // write on json
+    // write on json for python
     const dict_values = {currentAccount};
     const s = JSON.stringify(dict_values);
     console.log(s);
@@ -101,16 +88,5 @@ function handleAccountsChanged(accounts) {
   }
 }
 
-/**********************************************************/
-/* Handle chain (network) and chainChanged (per EIP-1193) */
-/**********************************************************/
-
-const chainId = await ethereum.request({ method: 'eth_chainId' });
-handleChainChanged(chainId);
-
-ethereum.on('chainChanged', handleChainChanged);
-
-function handleChainChanged(_chainId) {
-  // We recommend reloading the page, unless you must do otherwise
-  window.location.reload();
-}
+// Remove event listener
+ethereum.removeListener('accountsChanged', handleAccountsChanged);

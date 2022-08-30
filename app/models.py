f544fcb32d73602998c0c0d54a12247731a5e3a4
Matteo Razzanelli <matteo.razzanelli89@gmail.com>
2022-08-30 19:38:18 +0200

    feat: create model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+  
+class AuctionModelForm(models.Model):
+
+  TYPE = [
+    ('new', 'new'),
+    ('contribute', 'contribute'),
+  ]
+  STATUS = [
+    ('pending', 'pending'),
+    ('closed', 'closed'),
+  ]
+  
+  type = models.CharField(max_length=50, choices=TYPE)
+  id = models.FloatField(primary_key=True, blank=True, default=0)
+  description = models.CharField(blank=True, max_length=500, default=' ')
+  amount = models.FloatField(blank=True, default=0)
+  biggest_for_now = models.FloatField()
+  status = models.CharField(max_length=50, choices=STATUS, default='pending')

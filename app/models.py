528ee3f058150d803f2e40922c854726c4cc3f5e
Matteo Razzanelli <matteo.razzanelli89@gmail.com>
2022-09-04 19:34:02 +0200

    feat: add beneficiary and highest bidder addresses in model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 
 from django.db import models
+
+from django_ethereum.fields import EthereumAddressField
   
 class AuctionModelForm(models.Model):
 
@@ -14,7 +16,9 @@ class AuctionModelForm(models.Model):
   
   type = models.CharField(max_length=50, choices=TYPE)
   id = models.FloatField(primary_key=True, blank=True, default=0)
+  beneficiary = EthereumAddressField(blank=True, default='0x0000000000000000000000000000000000000000')
   description = models.CharField(blank=True, max_length=500, default=' ')
   amount = models.FloatField(blank=True, default=0)
   biggest_for_now = models.FloatField()
+  highest_bidder = EthereumAddressField(blank=True, default='0x0000000000000000000000000000000000000000')
   status = models.CharField(max_length=50, choices=STATUS, default='pending')

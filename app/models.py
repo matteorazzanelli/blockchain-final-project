7c7d8af9dc8ec98035e55df9724ae71cea3a7748
Matteo Razzanelli <matteo.razzanelli89@gmail.com>
2022-09-08 08:02:31 +0200

    refactor: added deadline to django model

--- app/models.py
+++ app/models.py
@@ -15,9 +15,10 @@ class AuctionModelForm(models.Model):
   ]
   
   type = models.CharField(max_length=50, choices=TYPE)
-  id = models.FloatField(primary_key=True, blank=True, default=0)
+  id = models.IntegerField(primary_key=True, blank=True, default=0)
   beneficiary = EthereumAddressField(blank=True, default='0x0000000000000000000000000000000000000000')
   description = models.CharField(blank=True, max_length=500, default=' ')
+  deadline = models.FloatField(blank=True, default=0) # since is unix epoch can be trated as float
   amount = models.FloatField(blank=True, default=0)
   biggest_for_now = models.FloatField()
   highest_bidder = EthereumAddressField(blank=True, default='0x0000000000000000000000000000000000000000')
